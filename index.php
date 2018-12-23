1e9db86f255160aaec961d0f2b50f568a940af3d
Bhavesh Kapre <undataker22@gmail.com>
2018-12-23 16:48:27 +0530

    individual display of posts modified 23/12/18 ::: 04:48 PM

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
                 $query = "select * from posts";
                 $sel_all_post = mysqli_query($connection,$query);
                 while ($row = mysqli_fetch_assoc($sel_all_post)) {
+                    $post_id = $row['post_id'];
                     $post_title = $row['post_title'];
                     $post_author = $row['post_author'];
                     $post_date = $row['post_date'];
@@ -29,7 +30,7 @@
 
                     <!-- First Blog Post -->
                     <h2>
-                        <a href="#"><?php echo $post_title; ?></a>
+                        <a href="post.php?post_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
                     </h2>
                     <p class="lead">
                         by <a href="index.php"><?php echo $post_author; ?></a>
@@ -39,7 +40,7 @@
                     <img class="img-responsive" src="image/<?php echo $post_image ?>" alt="">
                     <hr>
                     <p><?php echo $post_content; ?></p>
-                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    <a class="btn btn-primary" href="post.php?post_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                     <hr>
           <?php } ?>
